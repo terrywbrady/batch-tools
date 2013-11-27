dc30bd6fd0d18084e59683a034f21b45aabe06ef
Terry Brady <terrywbrady@gmail.com>
2013-11-27 12:58:08 -0500

    Add sortttable

--- web/header.php
+++ web/header.php
@@ -36,6 +36,7 @@ class LitHeader {
 		<script	src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
  		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
 		<script	src="{$WEBROOT}/web/qcReport.js"></script>
+		<script	src="{$WEBROOT}/web/sorttable.js"></script>
 		<link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/ui-lightness/jquery-ui.css"></link>
 		<link rel="stylesheet" type="text/css" href="{$WEBROOT}/web/qcReport.css"></link>
 		<title>{$this->title}</title>
