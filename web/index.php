b1d8e7d3c0280c04bb47a36d1a65645670bfe3ec
Terry Brady <terrywbrady@gmail.com>
2014-01-24 17:05:00 -0500

    test

--- web/index.php
+++ web/index.php
@@ -71,10 +71,8 @@ if ($CUSTOM->showStatsTools()) {
 <?php
 if ($CUSTOM->showBatchTools()) { 
 	echo $CUSTOM->getAdminHtml();
-	echo 11;
 }
 echo $CUSTOM->getOtherHtml();
-echo 22;
 
 function getQueryCols() {
 initQueries();
