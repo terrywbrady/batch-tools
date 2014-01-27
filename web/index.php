51d2d789d547e5d84e6d6b7b37b9ea1c6c3a17f6
Terry Brady <terrywbrady@gmail.com>
2014-01-27 10:15:06 -0500

    test

--- web/index.php
+++ web/index.php
@@ -24,6 +24,7 @@ include 'header.php';
 include 'query/queries.php';
 
 $CUSTOM = custom::instance();
+$CUSTOM->getCommunityInit()->initCommunities();
 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
