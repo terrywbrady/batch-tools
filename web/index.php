b78c1f92d0926533a542055277fa5897bbd98327
Terry Brady <terrywbrady@gmail.com>
2014-01-28 16:40:40 -0500

    set checked by default

--- web/index.php
+++ web/index.php
@@ -92,7 +92,7 @@ if (count($CUSTOM->getExcludeCollections()) > 0) {
     echo "<legend>Exclude Large Collections</legend>";
     foreach($CUSTOM->getExcludeCollections() as $k => $v) {
         echo <<< HERE
-      <input name="collex" type="checkbox" id="collex-{$k}" value="{$k}"><label for="collex-{$k}">{$v}</label>
+      <input name="collex" type="checkbox" id="collex-{$k}" value="{$k}" checked><label for="collex-{$k}">{$v}</label>
 HERE;
     }
 }
