19751d071f0b5086492c3b64e00baa7cf18cab6a
Terry Brady <terrywbrady@gmail.com>
2014-01-29 14:53:31 -0500

    test

--- bin-src/dspaceBatch.sh
+++ bin-src/dspaceBatch.sh
@@ -172,7 +172,7 @@ then
   curl "${SOLR}/search/update?stream.body=<update><delete><query>location.${SRCH}:${VAL}</query></delete><commit/></update>" >> ${RUNNING} 2>&1 
 
   echo "${DSROOT}/bin/dspace update-discovery-index" >> ${RUNNING} 2>&1 
-  #${DSROOT}/bin/dspace update-discovery-index >> ${RUNNING} 2>&1 
+  ${DSROOT}/bin/dspace update-discovery-index >> ${RUNNING} 2>&1 
 
   mv ${RUNNING} ${COMPLETE}
 else
