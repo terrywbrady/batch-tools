88687a28b20f4eeced72f0f2c3ba51fbd0cbcab6
Terry Brady <terrywbrady@gmail.com>
2014-01-29 17:15:08 -0500

    remove commit from command

--- bin-src/dspaceBatch.sh
+++ bin-src/dspaceBatch.sh
@@ -168,8 +168,8 @@ then
   
   echo Command: "$@" > ${RUNNING}
 
-  echo Command: curl "${SOLR}/search/update?stream.body=%3Cupdate%3E%3Cdelete%3E%3Cquery%3Elocation.${SRCH}:${VAL}%3C/query%3E%3C/delete%3E%3Ccommit/%3E%3C/update%3E" >> ${RUNNING} 2>&1 
-  curl "${SOLR}/search/update?stream.body=%3Cupdate%3E%3Cdelete%3E%3Cquery%3Elocation.${SRCH}:${VAL}%3C/query%3E%3C/delete%3E%3Ccommit/%3E%3C/update%3E" >> ${RUNNING} 2>&1 
+  echo Command: curl "${SOLR}/search/update?stream.body=%3Cupdate%3E%3Cdelete%3E%3Cquery%3Elocation.${SRCH}:${VAL}%3C/query%3E%3C/delete%3E%3C/update%3E" >> ${RUNNING} 2>&1 
+  curl "${SOLR}/search/update?stream.body=%3Cupdate%3E%3Cdelete%3E%3Cquery%3Elocation.${SRCH}:${VAL}%3C/query%3E%3C/delete%3E%3C/update%3E" >> ${RUNNING} 2>&1 
 
   echo "${DSROOT}/bin/dspace update-discovery-index" >> ${RUNNING} 2>&1 
   ${DSROOT}/bin/dspace update-discovery-index >> ${RUNNING} 2>&1 
