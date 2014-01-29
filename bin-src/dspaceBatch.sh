ad191d379034deee73bfb42af4cfddde89dfa866
Terry Brady <terrywbrady@gmail.com>
2014-01-29 13:31:54 -0500

    script

--- bin-src/dspaceBatch.sh
+++ bin-src/dspaceBatch.sh
@@ -166,8 +166,8 @@ then
   VAL=$3
   
   echo Command: "$@" > ${RUNNING}
-  echo Command: "run curl to remove index for location.${SRCH}:${VAL}" > ${RUNNING}
-  curl "http://localhost/solr/search/update?stream.body=&lt;update&gt;&lt;delete&gt;&lt;query&gt;location.${SRCH}:${VAL}&lt;/query&gt;&lt;/delete&gt;&lt;commit/&gt;&lt;/update>"
+  echo Command: curl "http://localhost/solr/search/update?stream.body=&lt;update&gt;&lt;delete&gt;&lt;query&gt;location.${SRCH}:${VAL}&lt;/query&gt;&lt;/delete&gt;&lt;commit/&gt;&lt;/update>" > ${RUNNING}
+  curl "http://localhost/solr/search/update?stream.body=&lt;update&gt;&lt;delete&gt;&lt;query&gt;location.${SRCH}:${VAL}&lt;/query&gt;&lt;/delete&gt;&lt;commit/&gt;&lt;/update>" > ${RUNNING}
   mv ${RUNNING} ${COMPLETE}
 else
   echo "Unsupported DSpace Command"
