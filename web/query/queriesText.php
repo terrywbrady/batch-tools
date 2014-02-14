b1bb28b4f0e867e91cb876c4c07d4b89c5aec003
Terry Brady <terrywbrady@gmail.com>
2014-02-14 17:07:36 -0500

    dup query name

--- web/query/queriesText.php
+++ web/query/queriesText.php
@@ -63,24 +63,7 @@ $subq = <<< EOF
         and bfr.mimetype in ('application/pdf')
     ) 
 EOF;
-new query("itemCountBadPdf","Large PDF (greater than 25M)",$subq,"text", new testValZero(),array("Accession","OrigName","SizeMB")); 
-
-$subq = <<< EOF
-    and exists 
-    (
-      select 1
-      from item2bundle i2b
-      inner join bundle b 
-        on i2b.bundle_id = b.bundle_id
-        and b.name = 'ORIGINAL'
-        and i.item_id = i2b.item_id
-      inner join bundle2bitstream b2b on b.bundle_id = b2b.bundle_id
-      inner join bitstream bit on bit.bitstream_id = b2b.bitstream_id
-      inner join bitstreamformatregistry bfr on bit.bitstream_format_id = bfr.bitstream_format_id
-        and bfr.mimetype in ('application/pdf')
-    ) 
-EOF;
-new query("xitemCountBadPdf","xLarge PDF (greater than 25M)",$subq,"text", new testValZero(),array("Accession","OrigName","SizeMB")); 
+new query("itemCountBigPdf","Large PDF (greater than 25M)",$subq,"text", new testValZero(),array("Accession","OrigName","SizeMB")); 
 
 $subq = <<< EOF
      and exists 
