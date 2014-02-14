1c24335a8e2cb20c2f409d0c41d60ddb38097d07
Terry Brady <terrywbrady@gmail.com>
2014-02-14 16:44:30 -0500

    fix subquery

--- web/query/queriesText.php
+++ web/query/queriesText.php
@@ -37,12 +37,13 @@ $subq = <<< EOF
       inner join bundle b 
         on i2b.bundle_id = b.bundle_id
         and b.name = 'ORIGINAL'
-        and i.item_id = i2b.item_id
       inner join bundle2bitstream b2b on b.bundle_id = b2b.bundle_id
       inner join bitstream bit on bit.bitstream_id = b2b.bitstream_id
         and bit.size_bytes < 20000
       inner join bitstreamformatregistry bfr on bit.bitstream_format_id = bfr.bitstream_format_id 
         and bfr.mimetype in ('application/pdf')
+      where 
+        i.item_id = i2b.item_id
     ) 
 EOF;
 new query("itemCountBadPdf","Possible Bad PDF (smaller than 20K)",$subq,"text", new testValZero(),array("Accession","OrigName","SizeKB")); 
@@ -55,12 +56,13 @@ $subq = <<< EOF
       inner join bundle b 
         on i2b.bundle_id = b.bundle_id
         and b.name = 'ORIGINAL'
-        and i.item_id = i2b.item_id
       inner join bundle2bitstream b2b on b.bundle_id = b2b.bundle_id
       inner join bitstream bit on bit.bitstream_id = b2b.bitstream_id
         and bit.size_bytes > 25000000
       inner join bitstreamformatregistry bfr on bit.bitstream_format_id = bfr.bitstream_format_id
         and bfr.mimetype in ('application/pdf')
+      where 
+        i.item_id = i2b.item_id
     ) 
 EOF;
 new query("itemCountBadPdf","Large PDF (greater than 25M)",$subq,"text", new testValZero(),array("Accession","OrigName","SizeMB")); 
