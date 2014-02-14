e22dca876dd0a11fc51cee9d027a7b0ae203d991
Terry Brady <terrywbrady@gmail.com>
2014-02-14 17:04:52 -0500

    test

--- web/query/queriesText.php
+++ web/query/queriesText.php
@@ -76,7 +76,8 @@ $subq = <<< EOF
         and i.item_id = i2b.item_id
       inner join bundle2bitstream b2b on b.bundle_id = b2b.bundle_id
       inner join bitstream bit on bit.bitstream_id = b2b.bitstream_id
-        and bfr.mimetype = 'application/pdf'
+      inner join bitstreamformatregistry bfr on bit.bitstream_format_id = bfr.bitstream_format_id
+        and bfr.mimetype in ('application/pdf')
     ) 
 EOF;
 new query("xitemCountBadPdf","xLarge PDF (greater than 25M)",$subq,"text", new testValZero(),array("Accession","OrigName","SizeMB")); 
