47219f1c12dd09802a8c94381acfad6afb04438c
Terry Brady <terrywbrady@gmail.com>
2014-03-04 17:31:19 -0500

    pdo style prep stmt

--- web/stats/getHandle.php
+++ web/stats/getHandle.php
@@ -11,11 +11,11 @@ if ($type == "0") {
 select h.handle 
 from handle h
 inner join item2bundle i2b on h.resource_id=i2b.item_id  
-inner join bundle2bitstream b2b on i2b.bundle_id=b2b.bundle_id and b2b.bitstream_id = $1
+inner join bundle2bitstream b2b on i2b.bundle_id=b2b.bundle_id and b2b.bitstream_id = :id
 where h.resource_type_id=2
 HERE;
 
-    $handle = $CUSTOM->getQueryVal($sql, array($id));
+    $handle = $CUSTOM->getQueryVal($sql, array(":id" => $id));
     if ($handle != "") {
 	    header("Location: /handle/" . $handle);
 	    exit;	
@@ -24,10 +24,10 @@ HERE;
 	$sql = <<< HERE
 select h.handle 
 from handle h
-where h.resource_id=$1 and h.resource_type_id=2
+where h.resource_id=:id and h.resource_type_id=2
 HERE;
 
-    $handle = $CUSTOM->getQueryVal($sql, array($id));
+    $handle = $CUSTOM->getQueryVal($sql, array(":id" => $id));
     if ($handle != "") {
 	    header("Location: /handle/" . $handle);
 	    exit;	
@@ -36,10 +36,10 @@ HERE;
 	$sql = <<< HERE
 select h.handle 
 from handle h
-where h.resource_id=$1 and h.resource_type_id=3
+where h.resource_id=:id and h.resource_type_id=3
 HERE;
 
-    $handle = $CUSTOM->getQueryVal($sql, array($id));
+    $handle = $CUSTOM->getQueryVal($sql, array(":id" => $id));
     if ($handle != "") {
 	    header("Location: /handle/" . $handle);
 	    exit;	
@@ -48,10 +48,10 @@ HERE;
 	$sql = <<< HERE
 select h.handle 
 from handle h
-where h.resource_id=$1 and h.resource_type_id=4
+where h.resource_id=:id and h.resource_type_id=4
 HERE;
 
-    $handle = $CUSTOM->getQueryVal($sql, array($id));
+    $handle = $CUSTOM->getQueryVal($sql, array(":id" => $id));
     if ($handle != "") {
 	    header("Location: /handle/" . $handle);
 	    exit;	
